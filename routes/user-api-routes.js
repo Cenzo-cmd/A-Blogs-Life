6d464bca094d7fcd74760c150ea506c8bde121aa
abraspin <abrahamspindel@gmail.com>
2020-10-27 17:12:33 -0500

    split api routes between blogpost and user, delete 'post-routes' (obsolete)

---
+++ routes/user-api-routes.js
@@ -0,0 +1,48 @@
+const db = require("../models");
+const passport = require("../config/passport");
+
+module.exports = (app) => {
+  //login in user with authentication
+  app.post("/api/login", passport.authenticate("local"), (request, response) => {
+    response.json(request.user);
+  });
+
+  // Create user
+  app.post("/api/signup", (request, response) => {
+    const { email, password, firstName, lastName } = request.body;
+    db.User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    })
+      .then(() => {
+        response.redirect(307, "/api/login");
+      })
+      .catch((err) => {
+        response.status(401).json(err);
+      });
+  });
+
+  // delete user
+  app.delete("/profile/:id", (request, response) => {
+    console.log(request.params);
+    db.User.destroy({
+      where: {
+        id: request.params.id,
+      },
+    })
+      .then((result) => {
+        response.json({ id: result });
+      })
+      .catch((err) => {
+        response.status(401).json(err);
+      });
+  });
+
+  //logout
+  app.get("/logout", (request, response) => {
+    request.logout();
+    response.redirect("/");
+  });
+};
