4ff7815df876d64a354915fa6b7b5c0398c75d5f
abraspin <abrahamspindel@gmail.com>
2020-10-28 20:49:50 -0500

    add blogpost-api-route reference

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const passport = require("passport");
 
-const db = require('./models');
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -19,9 +19,11 @@ app.set("view engine", "handlebars");
 
 require("./routes/html-routes")(app);
 require("./routes/user-api-routes")(app);
+require("./routes/blogPost-api-routes")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, () => {
-        console.log(`Server is live at http://localhost:${PORT}`);
-    });
+//TODO: force true to change data structures
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, () => {
+    console.log(`Server is live at http://localhost:${PORT}`);
+  });
 });
