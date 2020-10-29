8c6d529d9c2705d8d084b48b1d1569f82f1dfbc4
Vincent <vrdphone@gmail.com>
2020-10-29 17:16:39 -0500

    working on user login route

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,47 +1,33 @@
+//WORKING ON THIS ROUTE
+
 $(document).ready(function() {
     // $(".sidenav").sidenav(); // Materialize functionality for sidenav
 
     // Getting references to our form and inputs
-    let loginForm = $("#submit-button");
-    let emailInput = $("input#email-input");
-    let passwordInput = $("input#password-input");
-    let firstName = $("first-name");
-    let lastName = $("last-name");
+    let loginForm = $(".login");
+    let email = $("input#email-input");
+    let password = $("input#password-input");
 
     // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", (event) => {
-        console.log("you clicled me")
+
+    loginForm.on("submit", function(event) {
+        email = email.val().trim();
+        password = password.val().trim();
         event.preventDefault();
-        const userData = {
-            firstName: firstName.val().trim(),
-            lastName: lastName.val().trim(),
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim(),
-        };
-        console.log(userData);
 
-        if (!userData.email || !userData.password) {
-            return;
-        }
+        console.log(email, password);
 
-        // If we have an email and password we run the loginUser function and clear the form
-        loginUser(userData.email, userData.password);
-        emailInput.val("");
-        passwordInput.val("");
-    });
-
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
         $.post("/api/login", {
                 email: email,
                 password: password,
             })
             .then(function() {
-                window.location.replace("/members");
+                window.location.replace("/dashboard");
             })
             // If there's an error, log the error
             .catch(function(err) {
                 console.log(err);
             });
-    }
+
+    });
 });
