12266a946744556b87bc0b127e8be23125b1fa8d
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-10-28 21:13:41 -0500

    updated handlebars pages for separated js files, needs functionality

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,41 @@
-$(document).ready(function(){ 
-    // These functions are for materialize functionality
-    $('.sidenav').sidenav();
-    $('.tabs').tabs();
+$(document).ready(function () {
+  $(".sidenav").sidenav(); // Materialize functionality for sidenav
+
+  // Getting references to our form and inputs
+  let loginForm = $("form.login");
+  let emailInput = $("input#email-input");
+  let passwordInput = $("input#password-input");
+
+  // When the form is submitted, we validate there's an email and password entered
+  loginForm.on("submit", (event) => {
+    event.preventDefault();
+    const userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
+
+    // If we have an email and password we run the loginUser function and clear the form
+    loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password,
+    })
+      .then(function () {
+        window.location.replace("/members");
+      })
+      // If there's an error, log the error
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
 });
