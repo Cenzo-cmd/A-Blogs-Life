1d5c7ae1efc8e2c01f40bc0f3e645ae1d3d8b52f
abraspin <abrahamspindel@gmail.com>
2020-10-28 19:56:17 -0500

    add get routes for blog posts, findAll and findOne

--- routes/blogPost-api-routes.js
+++ routes/blogPost-api-routes.js
@@ -16,12 +16,31 @@ module.exports = (app) => {
         response.status(201).json(result);
       })
       .catch((err) => {
-        response.status(401).json(err);
+        response.status(400).json(err);
       });
   });
 
   ////////// R - Read - Get one or all posts
+  //GET ALL blogPosts associated with a particular user
+  app.get("/api/BlogPosts/:user_id", (request, response) => {
+    const query = {};
+    if (request.params.user_id) {
+      query.UserID = request.params.user_id;
+    }
+    db.BlogPost.findAll({
+      where: query,
+      include: [db.User],
+    }).then((dbPostResult) => {
+      response.json(dbPostResult);
+    });
+  });
 
+  // Get ONE BlogPost associated with a particular BlogPost_id
+  app.get("/api.BlogPosts/:BlogPost_id", (request, response) => {
+    db.BlogPost.findOne({ where: { id: req.params.id }, include: [db.User] }).then((dbPostResult) => {
+      response.json(dbPostResult);
+    });
+  });
   ////////// U - Update - Change title or content of post?
 
   ////////// D - Delete (Destroy) - Delete one or all posts
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,20 @@
 const path = require("path");
 
 module.exports = (app) => {
-<<<<<<< HEAD
-  app.get("/", function (request, response) {
-    response.json({ test: "This is the main route" });
+  // main route
+  app.get("/", (request, response) => {
+    // if (request.user) {
+    //     response.redirect("/members");
+    // }
+    response.render("index");
+    // response.sendFile(path.join(__dirname + "../views/index"))
+  });
+
+  app.get("/signup", (request, response) => {
+    response.render("signup");
+  });
+
+  app.get("/dashboard", (request, response) => {
+    response.render("dashboard");
   });
 };
-=======
-    // main route
-    app.get("/", (request, response) => {
-        // if (request.user) {
-        //     response.redirect("/members");
-        // }
-        response.render("index");
-        // response.sendFile(path.join(__dirname + "../views/index"))
-    });
-
-    app.get("/signup", (request, response) => {
-        response.render("signup");
-    });
-
-    app.get("/dashboard", (request, response) => {
-        response.render("dashboard");
-    })
-}
->>>>>>> 522f49530b667117ca37d2d5eb96fe68bc518852
