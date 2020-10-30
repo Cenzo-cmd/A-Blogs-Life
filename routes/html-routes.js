23ea2e0d040feb6e90adf717499f19b4a819f417
Vincent <vrdphone@gmail.com>
2020-10-30 17:21:56 -0500

    added new post html, updated nav bar

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,25 @@
 const path = require("path");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = (app) => {
-  // main route
-  app.get("/", (request, response) => {
-    // if (request.user) {
-    //     response.redirect("/members");
-    // }
-    response.render("index");
-    // response.sendFile(path.join(__dirname + "../views/index"))
-  });
+    // main route
+    app.get("/", (request, response) => {
+        // if (request.user) {
+        //     response.redirect("/members");
+        // }
+        response.render("index");
+        // response.sendFile(path.join(__dirname + "../views/index"))
+    });
 
-  app.get("/signup", (request, response) => {
-    response.render("signup");
-  });
+    app.get("/signup", (request, response) => {
+        response.render("signup");
+    });
 
-  app.get("/dashboard", (request, response) => {
-    response.render("dashboard");
-  });
+    app.get("/dashboard", isAuthenticated, (request, response) => {
+        response.render("dashboard");
+    });
+
+    app.get("/dashboard/newPost", isAuthenticated, (request, response) => {
+        response.render("newPost");
+    })
 };
