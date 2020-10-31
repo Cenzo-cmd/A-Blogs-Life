d051f933acce5bf470fb7f815eb0b0e506dafe8f
Vincent <vrdphone@gmail.com>
2020-10-31 12:13:47 -0500

    fixed linting problems

--- public/assets/js/updateProfile.js
+++ public/assets/js/updateProfile.js
@@ -1,37 +1,37 @@
-$(document).ready(function () {
-  $(".sidenav").sidenav(); // Materialize functionality for sidenav
-  $(".tabs").tabs(); // Materialize functionality for tabs on profile
+$(document).ready(() => {
+    $(".sidenav").sidenav(); // Materialize functionality for sidenav
+    $(".tabs").tabs(); // Materialize functionality for tabs on profile
 
-  //materialize prefill
-  M.updateTextFields();
+    //materialize prefill
+    M.updateTextFields();
 
-  let firstNameField = $("#first_name");
-  let lastNameField = $("#last_name");
-  let emailField = $("#email");
-  populateProfileFields();
+    const firstNameField = $("#first_name");
+    const lastNameField = $("#last_name");
+    const emailField = $("#email");
+    populateProfileFields();
 
-  function populateProfileFields() {
-    console.log("I'm trying to get user data!");
-    $.get("/api/users/userdata", (userData) => {
-      $("#first_name").text = userData.firstName;
-      $("#last_name").text = userData.lastName;
-      $("#email").text = userData.email;
+    function populateProfileFields() {
+        console.log("I'm trying to get user data!");
+        $.get("/api/users/userdata", (userData) => {
+            $("#first_name").text = userData.firstName;
+            $("#last_name").text = userData.lastName;
+            $("#email").text = userData.email;
+        });
+    }
+
+    $("#update-profile-form").on("submit", (event) => {
+        event.preventDefault();
+        const updateQuery = {
+            firstName: firstNameField.val().trim(),
+            lastName: lastNameField.val().trim(),
+            email: emailField.val().trim(),
+        };
+        //   $.put("/api/users/:id", updateQuery, (results)=>{
+        //       results.
+        //   })
+
+        $.ajax("/api/users/", { method: "PUT", data: updateQuery }).then(() => {
+            window.location.replace("/dashboard");
+        });
     });
-  }
-
-  $("#update-profile-form").on("submit", (event) => {
-    event.preventDefault();
-    const updateQuery = {
-      firstName: firstNameField.val().trim(),
-      lastName: lastNameField.val().trim(),
-      email: emailField.val().trim(),
-    };
-    //   $.put("/api/users/:id", updateQuery, (results)=>{
-    //       results.
-    //   })
-
-    $.ajax("/api/users/", { method: "PUT", data: updateQuery }).then((results) => {
-      window.location.replace("/dashboard");
-    });
-  });
 });
