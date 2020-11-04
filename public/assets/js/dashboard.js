405a9b303b268c6725b42dc1bcacdd28ff9208df
abraspin <abrahamspindel@gmail.com>
2020-11-04 14:14:34 -0600

    add 'close without saving' restriction to post edit

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,39 +1,46 @@
 $(document).ready(() => {
-    $(".sidenav").sidenav(); // Materialize functionality for sidenav
-    $(".tabs").tabs(); // Materialize functionality for tabs on profile
+  $(".sidenav").sidenav(); // Materialize functionality for sidenav
+  $(".tabs").tabs(); // Materialize functionality for tabs on profile
 
-    document.title = "It's a Blog's Life";
+  document.title = "It's a Blog's Life";
 
-    M.Modal.init(document.querySelectorAll(".modal"));
-    $(".modal").modal();
-    M.updateTextFields();
+  M.updateTextFields();
+  $(".modal").modal({
+    dismissible: false, // Modal cannot be closed by clicking anywhere outside
+  });
 
-    // //event listener for "submit post edits" buttons
-    $(document).on("submit", ".update-blogPost-form", (event) => {
-        event.preventDefault();
-        // const blogId = $(this).attr("id").split("-")[1];
-        const blogId = event.currentTarget.dataset.id;
+  // reload page if exiting post edit modal
+  $(".close-modal-button").click(() => {
+    console.log("closing without saving!");
+    window.location.reload();
+  });
 
-        const updateQuery = {
-            title: $(`#title-${blogId}`).val(),
-            body: $(`#body-${blogId}`).val(),
-            // eslint-disable-next-line camelcase
-            blogPost_id: blogId,
-        };
-        console.log("updateQuery", updateQuery);
+  // //event listener for "submit post edits" buttons
+  $(document).on("submit", ".update-blogPost-form", (event) => {
+    event.preventDefault();
+    // const blogId = $(this).attr("id").split("-")[1];
+    const blogId = event.currentTarget.dataset.id;
 
-        $.ajax("/api/blogposts", { method: "PUT", data: updateQuery }).then(() => {
-            window.location.reload();
-        });
+    const updateQuery = {
+      title: $(`#title-${blogId}`).val(),
+      body: $(`#body-${blogId}`).val(),
+      // eslint-disable-next-line camelcase
+      blogPost_id: blogId,
+    };
+    console.log("updateQuery", updateQuery);
+
+    $.ajax("/api/blogposts", { method: "PUT", data: updateQuery }).then(() => {
+      window.location.reload();
     });
+  });
 
-    // //event listener for "DELETE post" buttons
-    $(document).on("click", ".delete-post-btn", (event) => {
-        event.preventDefault();
-        const blogId = event.currentTarget.dataset.postid;
+  // //event listener for "DELETE post" buttons
+  $(document).on("click", ".delete-post-btn", (event) => {
+    event.preventDefault();
+    const blogId = event.currentTarget.dataset.postid;
 
-        $.ajax("api/BlogPosts/" + blogId, { method: "DELETE" }).then(() => {
-            window.location.reload();
-        });
+    $.ajax("api/BlogPosts/" + blogId, { method: "DELETE" }).then(() => {
+      window.location.reload();
     });
+  });
 });
